b0b58598b583600f17df80bb9a6def207ab7d44e
jerricknsc <jerricknsc@gmail.com>
2024-06-01 01:20:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
-const app = express();
-const PORT = 8001;
+const app = express(); // CREATING AN INSTANCE OF EXPRESS
+app.use(express.json()); // TELLING EXPRESS TO UNDERSTAND JSON
+app.use(cors()); // IM NOT VERY SURE WHAT THIS DOES LOL, HAVE TO FIND OUT 😅
+
+const PORT = 8001; // DEFINING OUR PORT AS 8001
 
 // HEALTH CHECK ENDPOINT
 app.get('/', (req, res) => {
