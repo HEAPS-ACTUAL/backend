839cc0f49bde72107ce8db68372410eeb65ce444
jerricknsc <jerricknsc@gmail.com>
2024-06-01 02:14:36 +0800

    added authenticate

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -12,6 +12,11 @@ function getAllUsers(req, res){
     return res.status(200).json(allUsers);
 }
 
+function authenticate(req, res){
+    const request_body = req.body;
+    return res.status(200).json(request_body); 
+}
+
 
 // EXPORT ALL THE FUNCTIONS
-module.exports = {getAllUsers};
+module.exports = {getAllUsers, authenticate};
--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers } = require('../controllers/UserController');
+const { getAllUsers, authenticate } = require('../controllers/UserController');
 
 // GET ALL USERS
 router.get('/', getAllUsers);
 
 // AUTHENTICATE
+router.post('/authenticate', authenticate);
 
 module.exports = router;
