c465eca68b6e85e2d77b4202eabebb0fec4a05fa
jerricknsc <jerricknsc@gmail.com>
2024-06-01 01:10:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const express = require('express');
 
 const app = express();
+const PORT = 8001;
 
+// HEALTH CHECK ENDPOINT
+app.get('/', (req, res) => {
+    return res.status(200).json({message: "Server is up and running!"});
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+})
