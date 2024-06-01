64f0c0cffa322679ac8ca50e162cd78fbee0fe30
jerricknsc <jerricknsc@gmail.com>
2024-06-01 15:44:38 +0800

    added comments for promisify.js

--- utils/Promisify.js
+++ utils/Promisify.js
@@ -1,3 +1,9 @@
+/*
+The con.query function is orignally supposed to take in a callback function and execute it.
+The purpose of this file is to change con.query into a function that returns the results of the SQL query directly instead of 
+carrying out the callback function. This makes the function easier to deal with and results in neater code. 
+*/
+
 const util = require('util');
 const con = require('../models/ConnectionManager');
 
