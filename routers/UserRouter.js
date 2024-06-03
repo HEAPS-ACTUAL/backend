eb5984ba08fdd44878e9a4bac2ac808494aa2244
jerricknsc <jerricknsc@gmail.com>
2024-06-04 01:43:31 +0800

    Update UserRouter.js

--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -1,5 +1,6 @@
 /*
-This file is in charge of determining which functions from UserController.js to execute based on the endpoint of the request that is being sent.
+This file is in charge of determining which functions from UserController.js to 
+execute based on the endpoint of the request that is being sent.
 */
 
 const express = require('express');
