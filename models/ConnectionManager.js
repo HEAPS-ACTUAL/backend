e3759fa1641b7ba6f9a762a7350de97e07018412
jerricknsc <jerricknsc@gmail.com>
2024-06-07 23:07:03 +0800

    added BD password to git ignore

---
+++ models/ConnectionManager.js
@@ -0,0 +1,14 @@
+/* 
+Creating an connection instance to mySQL database.
+*/
+const mysql = require("mysql");
+const DB_password = require("./Password _for_DB");
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: DB_password,
+  database: "heap",
+});
+
+module.exports = con;
