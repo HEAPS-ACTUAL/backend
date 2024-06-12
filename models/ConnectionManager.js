d403904d220ede2d86f187d3993ceb197407c4e5
jerricknsc <jerricknsc@gmail.com>
2024-06-12 14:07:26 +0800

    Update ConnectionManager.js

--- models/ConnectionManager.js
+++ models/ConnectionManager.js
@@ -2,7 +2,7 @@
 Creating an connection instance to mySQL database.
 */
 const mysql = require("mysql");
-const DB_password = require("./Password _for_DB");
+const DB_password = require("./Password_for_DB");
 
 const con = mysql.createConnection({
     host: "localhost",
