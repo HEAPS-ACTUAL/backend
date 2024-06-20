418db8cbc1ffeb289c97ae05972900ea84462680
jerricknsc <jerricknsc@gmail.com>
2024-06-21 00:14:13 +0800

    completed addAllQuestionsForATest

--- controllers/QuestionController.js
+++ controllers/QuestionController.js
@@ -5,36 +5,38 @@ const query = require("../utils/PromisifyQuery");
 SQL DATABASE RELATED FUNCTIONS
 ------------------------------------------------------------------------------------------------------------------------------------
 */
-async function addNewQuestion(email, quizID, questionText, elaboration) {
-    try {
-        const questionNo = (await countTotalNumberOfQuestions(null, null, email, quizID)) + 1;
-        const sqlQuery = "Insert into question (UserEmail, QuizID, QuestionNo, QuestionText, Elaboration) values (?, ?, ?, ?, ?)";
-        const insertOk = await query(sqlQuery, [email, quizID, questionNo, questionText, elaboration]);
 
-        if (insertOk) {
-            console.log(`Question ${questionNo} added!`);
-            return questionNo;
+async function addAllQuestionsForATest(arrayOfValues){
+    try{
+        const sqlQuery = "Insert into question (TestID, QuestionNo, QuestionText, Elaboration) values ?";
+        const insertOk = await query(sqlQuery, [arrayOfValues]);
+        
+        if(insertOk.affectedRows === arrayOfValues.length){
+            console.log('All questions for this quiz has been inserted!');
         }
-    } catch (error) {
-        const msg = `Error adding question ${questionNo} into database`;
+        else{
+            console.log('Not all questions have been inserted!');
+        }
+    }
+    catch(error){
+        const msg = `Error adding questions into database`;
         console.error(`${msg}: ${error.message}`);
         throw new Error(msg);
     }
 }
 
-async function countTotalNumberOfQuestions(req, res, email = null, quizID = null) {
+async function countTotalNumberOfQuestions(req, res, quizID = null) {
     try {
-        const sqlQuery = "select count(*) as numOfQuestions from question where UserEmail = ? and QuizID = ?";
+        const sqlQuery = "select count(*) as numOfQuestions from question where TestID = ?";
         
         if(req && res){
-            const email = req.body.email;
-            const quizID = req.body.quizID;
-            const returnedData = await query(sqlQuery, [email, quizID]);
+            const testID = req.body.testID;
+            const returnedData = await query(sqlQuery, [testID]);
             const numOfQuestions = returnedData[0].numOfQuestions;
             return res.status(200).json(numOfQuestions);
         }
-        else if(email && quizID){
-            const returnedData = await query(sqlQuery, [email, quizID]);
+        else if(testID){
+            const returnedData = await query(sqlQuery, [testID]);
             const numOfQuestions = returnedData[0].numOfQuestions;
             return numOfQuestions;
         }
@@ -43,16 +45,16 @@ async function countTotalNumberOfQuestions(req, res, email = null, quizID = null
     }
 }
 
-async function getLastTwoQuestions(email, quizID) {
+async function getLastTwoQuestions(testID) {
     try {
         const sqlQuery = `
             SELECT *
             FROM Question
-            WHERE UserEmail = ? AND QuizID = ?
+            WHERE TestID = ?
             ORDER BY QuestionNo DESC
             LIMIT 2;
         `;
-        const results = await query(sqlQuery, [email, quizID]);
+        const results = await query(sqlQuery, [testID]);
         return results.reverse(); // Reverse to maintain the order of insertion when displaying
     }
     catch (error) {
@@ -62,9 +64,7 @@ async function getLastTwoQuestions(email, quizID) {
 }
 
 async function getAllQuestionsAndOptionsFromAQuiz(req, res){
-    const email = req.body.email;
     const quizID = req.body.quizID;
-    // const email = 'jerricknsc@gmail.com';
     // const quizID = 1;
 
     try{
