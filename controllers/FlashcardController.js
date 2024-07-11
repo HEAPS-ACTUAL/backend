4a9ce2cbc3959d2d97544033fab19b9ce8b01b14
jerricknsc <jerricknsc@gmail.com>
2024-07-11 13:14:44 +0800

    Update FlashcardController.js

--- controllers/FlashcardController.js
+++ controllers/FlashcardController.js
@@ -1,39 +1,33 @@
 const query = require("../utils/PromisifyQuery");
 
 async function getAllFlashcardsWithoutSchedule(req, res) {
-  const email = req.body.email;
+    const email = req.body.email;
 
-  try {
-    const sqlQuery =
-      'Select TestID as value, TestName as label from Test where (Email = ?) and (TestType = "F") and isnull(ScheduleID)';
-    const returnedData = await query(sqlQuery, [email]);
-    res.status(200).json(returnedData);
-  } catch (error) {
-    const msg = `Error retrieving flashcards from DB!`;
-    console.error(`${msg}: ${error.message}`);
-    res.status(404).json({ message: msg });
-  }
+    try {
+        const sqlQuery = 'Select TestID as value, TestName as label from Test where (Email = ?) and (TestType = "F") and isnull(ScheduleID)';
+        const returnedData = await query(sqlQuery, [email]);
+        res.status(200).json(returnedData);
+    } 
+    catch (error) {
+        const msg = `Error retrieving flashcards from DB!`;
+        console.error(`${msg}: ${error.message}`);
+        res.status(404).json({ message: msg });
+    }
 }
 
 async function getFlashcardsByScheduleID(req, res) {
-  const scheduleID = req.body.scheduleID;
+    const scheduleID = req.body.scheduleID;
 
-  try {
-    const sqlQuery =
-      "Select TestID, TestName, DateTimeCreated from Test where ScheduleID = ?";
-    const returnedData = await query(sqlQuery, [scheduleID]);
+    try {
+        const sqlQuery = "Select TestID, TestName, DateTimeCreated from Test where ScheduleID = ?";
+        const returnedData = await query(sqlQuery, [scheduleID]);
 
-    const formattedData = returnedData.map((item) => ({
-      TestID: item.TestID,
-      TestName: item.TestName,
-      DateTimeCreated: new Date(item.DateTimeCreated).toISOString(), // Ensure the date is properly formatted
-    }));
-
-    res.status(200).json(formattedData);
-  } catch (error) {
-    console.error("Failed to fetch tests:", error);
-    res.status(404).json({ message: error });
-  }
+        res.status(200).json(returnedData);
+    }
+    catch (error) {
+        console.error("Failed to fetch tests:", error);
+        res.status(404).json({ message: error });
+    }
 }
 
 module.exports = { getAllFlashcardsWithoutSchedule, getFlashcardsByScheduleID };
