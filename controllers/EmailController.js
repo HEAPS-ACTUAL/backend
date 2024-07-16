a74183a7d7de7c4cfd0d7a6b871a547f56f57821
shihuifong <146155096+shihuifong@users.noreply.github.com>
2024-07-16 09:20:46 +0800

    fixed msg used before being defined

--- controllers/EmailController.js
+++ controllers/EmailController.js
@@ -1,6 +1,6 @@
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-const nodemailer = require('nodemailer');
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+const nodemailer = require("nodemailer");
 
 // .env CONFIGS
 const HOSTNAME = process.env.HOSTNAME;
@@ -16,54 +16,52 @@ creates the SMTP service that will be used to send emails
 we are using an app password by google in the .env file
 */
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        type: "login",
-        user: APP_EMAIL,
-        pass: APP_EMAIL_PASSWORD,
-    },
+  service: "gmail",
+  auth: {
+    type: "login",
+    user: APP_EMAIL,
+    pass: APP_EMAIL_PASSWORD,
+  },
 });
 
 //  Tests whether quizDaddy email is connected
 transporter.verify((error, success) => {
-    if (error) {
-        console.log(error);
-    }
-    else {
-        console.log("Server is ready to send out emails");
-        // sendVerificationEmail();
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to send out emails");
+    // sendVerificationEmail();
+  }
 });
 
 async function sendVerificationEmail(req, res = null) {
-    const inputEmail = req.body.email;
-    const token = generateVerificationToken(inputEmail);
+  const inputEmail = req.body.email;
+  const token = generateVerificationToken(inputEmail);
 
-    try {
-        const verificationLink = `http://${HOSTNAME}:${FE_PORT}/verify-email?token=${token}`
-        
-        const info = await transporter.sendMail({
-            from: `"quizDaddy" <${APP_EMAIL}>`, // sender address
-            to: inputEmail, // list of receivers
-            subject: "Email Verification for QuizDaddy", // Subject line
-            // text: `Visit this link to verify your email: ${verificationLink}`,  // plain text body
-            html: `<p>Hello! <br><br> Thank you for choosing quizDaddy. <br><br> To complete your registration, please <a href="${verificationLink}">verify your email.<p> </a>Best regards, <br> The Quizdaddy Team`, // html body
-        });
+  try {
+    const verificationLink = `http://${HOSTNAME}:${FE_PORT}/verify-email?token=${token}`;
 
-        console.log(`Verification email sent successfully to ${inputEmail}. The message ID is: ${info.messageId}`);
-        
-        if (res == null){
-            return msg;
-        }
-        else{
-            return res.status(200).json({message: msg}) 
-        }
+    const info = await transporter.sendMail({
+      from: `"quizDaddy" <${APP_EMAIL}>`, // sender address
+      to: inputEmail, // list of receivers
+      subject: "Email Verification for QuizDaddy", // Subject line
+      // text: `Visit this link to verify your email: ${verificationLink}`,  // plain text body
+      html: `<p>Hello! <br><br> Thank you for choosing quizDaddy. <br><br> To complete your registration, please <a href="${verificationLink}">verify your email.<p> </a>Best regards, <br> The Quizdaddy Team`, // html body
+    });
+
+    const msg = `Verification email sent successfully to ${inputEmail}. The message ID is: ${info.messageId}`;
+    console.log(msg);
+
+    if (res == null) {
+      return msg;
+    } else {
+      return res.status(200).json({ message: msg });
     }
-    catch (error) {
-        const msg = `Error sending verification email`;
-        console.error(`${msg}: ${error.message}`);
-        throw new Error(msg);
-    }
+  } catch (error) {
+    const msg = `Error sending verification email`;
+    console.error(`${msg}: ${error.message}`);
+    throw new Error(msg);
+  }
 }
 
 module.exports = { sendVerificationEmail };
