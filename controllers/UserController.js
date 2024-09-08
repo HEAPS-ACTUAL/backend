e72948afbb5e308cc798ddb7835bf50eaa20be28
Isaiah Chia <130481550+Isaidchia@users.noreply.github.com>
2024-09-08 20:40:25 +0800

    test

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -19,9 +19,9 @@ differently depending on who is calling it (see if statement below).
 */
 
 async function getUserByEmail(req, res = null) {
-    console.log(`Request Body: ${req.body}`);
-    console.log(`Request Query: ${req.query}`);
-    console.log(`Request Params: ${req.params}`);
+    console.log(req.body);
+    console.log(req.query);
+    console.log(req.params);
     const inputEmail = req.body.email || req.query.email || req.params.email;
     const sqlQuery = "Select Email, HashedPassword, FirstName, LastName, Gender, convert(DateTimeJoined, char) as DateTimeJoined, IsVerified from User where Email = ?";
 
